6cb9df4b2c2a3e63df92677b9b5a9c0ab45568d2
renestimu <byumitsiz@gmail.com>
2022-08-03 09:36:00 +0300

    Güncelleme 3: Repository'ler oluşturuldu.

---
+++ PhoneBookAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using PhoneBookAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookAPI.Application.Repositories
+{
+    public interface IReadRepository<T>:IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll(bool traking = true);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool traking = true);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool traking = true);
+        Task<T> GetByIdAsync(string id, bool traking = true);
+    }
+}
---
+++ PhoneBookAPI.Application/Repositories/Report/IReportWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using PhoneBookAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookAPI.Application.Repositories
+{
+    public interface IReportWriteRepository:IWriteRepository<Report>
+    {
+    }
+}
