0fb5f26c6cf3cf8d696f68852f45745af1740dd1
renestimu <byumitsiz@gmail.com>
2022-08-04 14:04:36 +0300

    Güncelleme 7: İletişim ve rapor alanları hazırlandı

---
+++ PhoneBookAPI.API/Controllers/CommunicationController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBookAPI.Application.Repositories;
+using PhoneBookAPI.Domain.Entities;
+using PhoneBookAPI.Persistence.Repositories;
+
+namespace PhoneBookAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunicationController : ControllerBase
+    {
+        private readonly ICommunicationReadRepository _communicationReadRepository;
+        private readonly ICommunicationWriteRepository _communicationWriteRepository;
+        readonly private IContactReadRepository _contactReadRepository;
+
+        public CommunicationController(CommunicationReadRepository communicationReadRepository,CommunicationWriteRepository communicationWriteRepository, IContactReadRepository contactReadRepository)
+        {
+            _communicationReadRepository = communicationReadRepository;
+            _communicationWriteRepository = communicationWriteRepository;
+            _contactReadRepository = contactReadRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Communication model,string id)
+        {
+
+            Contact contact = await _contactReadRepository.GetByIdAsync(id);
+
+            await _communicationWriteRepository.AddAsync(new()
+            {
+                Contact = contact,
+                Email = model.Email,
+                Information = model.Information,
+                PhoneNumber = model.PhoneNumber,
+                Location = model.Location,
+         
+            });
+            await _communicationWriteRepository.SaveAsync();
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            await _communicationWriteRepository.RemoveAsync(id);
+            await _communicationWriteRepository.SaveAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ PhoneBookAPI.API/Controllers/ReportController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBookAPI.Application.Repositories;
+using PhoneBookAPI.Persistence.Repositories;
+
+namespace PhoneBookAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        readonly private IReportWriteRepository _reportWriteRepository;
+        readonly private IReportReadRepository _reportReadRepository;
+
+        public ReportController(ReportWriteRepository reportWriteRepository,ReportReadRepository reportReadRepository)
+        {
+            _reportWriteRepository = reportWriteRepository;
+            _reportReadRepository = reportReadRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(_reportReadRepository.GetAll(false));
+        }
+    }
+}
